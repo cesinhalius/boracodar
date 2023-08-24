574e35799b001b21501e344023abf510f8642aef
Cesar Augusto <cesinhalius@hotmail.com>
2023-08-24 18:27:27 -0300

    Projeto de aba de pagamento de e-commerce

---
+++ desafio_21/script.js
@@ -0,0 +1,13 @@
+let cont = document.getElementById("quant_item");
+
+let num = 0;
+
+function somar() {
+  num++;
+  cont.innerText = num;
+}
+
+function decrementar() {
+  num--;
+  cont.innerText = num;
+}
